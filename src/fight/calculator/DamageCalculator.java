5c2893d3fcd766fc2771d9d2428774396c3a84d2
Aleksey Krivotulov <Hurricane095@gmail.com>
2017-12-06 21:26:42 +0300

    DamageCalculator

--- src/fight/calculator/DamageCalculator.java
+++ src/fight/calculator/DamageCalculator.java
@@ -10,13 +10,25 @@ public class DamageCalculator { // для боя 1 на 1
 
     private static final Double DAMAGE_COEFFICIENT=0.4;
     private static final Double POWER_COEFFICIENT=0.6;
-
-    public double damageOnHit(Weapon weaponDefense, Gladiator gladiatorAttack,double criticalDamage, double armor, double wearArmor){
-        double dmg=damage(weaponDefense,gladiatorAttack);
-        return dmg+dmg*criticalDamage - armor*wearArmor;
+    private double learnability;
+    public DamageCalculator(double learnability){
+        this.learnability=learnability;
     }
-    public double damage(Weapon weaponDefense, Gladiator gladiatorAttack){
 
-        return DAMAGE_COEFFICIENT*weaponDefense.getDamage()+POWER_COEFFICIENT*gladiatorAttack.getCalculatedProperties().getImpactForce();
+    public double damageOnHit(Gladiator gladiatorDefense, Gladiator gladiatorAttack){
+        double dmg=damage(gladiatorAttack.getWeapon(),gladiatorAttack.getCalculatedProperties().getImpactForce(),gladiatorAttack.getImprovedProperties().getPossessionOfArms());
+        return dmg+dmg*gladiatorAttack.getCalculatedProperties().getCritical()
+                - gladiatorDefense.getArmor().getProtection()*gladiatorDefense.getArmor().getWear();
+    }
+    public double damage(Weapon weapon, Integer force,Integer possessionOfArms){
+        double damageWeapon=damageWeapon(weapon);
+        double impactForce=impactForce(force,possessionOfArms);
+        return DAMAGE_COEFFICIENT*damageWeapon+POWER_COEFFICIENT*impactForce;
+    }
+    public double damageWeapon(Weapon weapon){
+        return weapon.getDamage()*(weapon.getWear()+learnability/100);
+    }
+    public double impactForce(Integer damage,Integer skill){
+        return damage*(skill/100+learnability/100);
     }
 }
